008769bdcc0ee2b6d353865ea0acb541b97f5f74
cafemoca <dev@moca.cm>
2018-06-22 02:05:32 +0900

    Initial commit

---
+++ src/main/kotlin/cm/moca/work/biblio/BiblioApplication.kt
@@ -0,0 +1,11 @@
+package cm.moca.work.biblio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiblioApplication
+
+fun main(args: Array<String>) {
+    runApplication<BiblioApplication>(*args)
+}
---
+++ src/test/kotlin/cm/moca/work/biblio/BiblioApplicationTests.kt
@@ -0,0 +1,16 @@
+package cm.moca.work.biblio
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BiblioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
